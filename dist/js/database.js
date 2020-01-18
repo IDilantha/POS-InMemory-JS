ed513e2982b6230aaee0cf78ea80bae6c6c48820
IDilantha <isurudilantha@gmail.com>
2020-01-18 14:59:57 +0530

    Add data to database.js file

--- dist/js/database.js
+++ dist/js/database.js
@@ -1,7 +1,119 @@
-var customers = [];
+var customers = [
+    {
+        id : "C001",
+        name : "Alisa",
+        address : "Pinwatte"
+    },
+    {
+        id : "C002",
+        name : "Pande",
+        address : "Balangoda"
+    },
+    {
+        id : "C003",
+        name : "Waduma",
+        address : "Horana"
+    },
+    {
+        id : "C004",
+        name : "Pera",
+        address : "Pandura"
+    },
+    {
+        id : "C005",
+        name : "Punchiya",
+        address : "Borella"
+    }
+];
 
-var items = [];
+var items = [
+    {
+        code: "I001",
+        description:"Mouse",
+        qty:"10",
+        unitPrice:"500"
+    },
+    {
+        code: "I002",
+        description:"Keyboard",
+        qty:"25",
+        unitPrice:"1000"
+    },
+    {
+        code: "I003",
+        description:"Headphone",
+        qty:"15",
+        unitPrice:"2500"
+    },
+    {
+        code: "I004",
+        description:"Pen Drive",
+        qty:"20",
+        unitPrice:"1500"
+    },
+    {
+        code: "I005",
+        description:"Phone Charger",
+        qty:"10",
+        unitPrice:"750"
+    }
+];
 
-var orders = [];
-
-var orderDetails = [];
+var orders = [
+    {
+        orderId:"OD001",
+        orderDate:"2020-01-10",
+        customerId:"C001",
+        customerName:"Alisa",
+        total:"3000"
+    },
+    {
+        orderId:"OD002",
+        orderDate:"2020-01-11",
+        customerId:"C002",
+        customerName:"Pande",
+        total:"2500"
+    },
+    {
+        orderId:"OD003",
+        orderDate:"2020-01-11",
+        customerId:"C003",
+        customerName:"Waduma",
+        total:"750"
+    },
+    {
+        orderId:"OD004",
+        orderDate:"2020-01-12",
+        customerId:"C004",
+        customerName:"Pera",
+        total:"4500"
+    },
+    {
+        orderId:"OD005",
+        orderDate:"2020-01-14",
+        customerId:"C005",
+        customerName:"Punchiya",
+        total:"7500"
+    },
+    {
+        orderId:"OD006",
+        orderDate:"2020-01-14",
+        customerId:"C003",
+        customerName:"Waduma",
+        total:"5000"
+    },
+    {
+        orderId:"OD007",
+        orderDate:"2020-01-15",
+        customerId:"C002",
+        customerName:"Pande",
+        total:"10000"
+    },
+    {
+        orderId:"OD008",
+        orderDate:"2020-01-17",
+        customerId:"C001",
+        customerName:"Alisa",
+        total:"2500"
+    }
+];
