075df393d9c7aa72f6959b21b9d31f87b7c243ff
IDilantha <isurudilantha@gmail.com>
2020-01-19 13:36:15 +0530

    Complete search-order

--- dist/js/manage-search_orders-controller.js
+++ dist/js/manage-search_orders-controller.js
@@ -15,3 +15,24 @@ function loadOrders() {
         $("#tbl-orders tbody").append(html);
     }
 }
+
+$("#txtSearch").keyup(function () {
+    var txt = $("#txtSearch").val();
+    $("#tbl-orders tbody tr").remove();
+    search(txt);
+});
+
+function search() {
+    for(var i=0; i < orders.length; i++){
+        if (arguments[0]== orders[i].orderId || arguments[0]==orders[i].orderDate || arguments[0]==orders[i].customerId || arguments[0]==orders[i].customerName || arguments[0]==orders[i].total ){
+             var search = '<tr>' +
+                 '<td>'+ orders[i].orderId+'</td>' +
+                 '<td>'+ orders[i].orderDate+'</td>' +
+                 '<td>'+ orders[i].customerId+'</td>' +
+                 '<td>'+ orders[i].customerName+'</td>' +
+                 '<td>'+ orders[i].total+'</td>' +
+                 '</tr>';
+             $("#tbl-orders tbody").append(search);
+        }
+    }
+}
